f0e7b10b8756bb89b84db7b474e01282301ae391
Kristian Stöckel <git@k118.de>
2024-11-15 16:44:17 +0100

    add todo

--- app/Services/PersonalDataSelection/UserGdprDataService.php
+++ app/Services/PersonalDataSelection/UserGdprDataService.php
@@ -41,46 +41,47 @@ class UserGdprDataService
 
         $personalDataSelection
             ->add('user.json', $userData)
-            ->add('notifications.json', $userModel->notifications()->get()->toJson())
-            ->add('likes.json', $userModel->likes()->get()->toJson())
-            ->add('social_profile.json', $userModel->socialProfile()->with('mastodonserver')->get())
-            ->add('event_suggestions.json', EventSuggestion::where('user_id', $userModel->id)->get()->toJson())
-            ->add('events.json', Event::where('approved_by', $userModel->id)->get()->toJson())
+            ->add('notifications.json', $userModel->notifications()->get()->toJson()) //TODO: columns definieren
+            ->add('likes.json', $userModel->likes()->get()->toJson()) //TODO: columns definieren
+            ->add('social_profile.json', $userModel->socialProfile()->with('mastodonserver')->get()) //TODO: columns definieren
+            ->add('event_suggestions.json', EventSuggestion::where('user_id', $userModel->id)->get()->toJson()) //TODO: columns definieren
+            ->add('events.json', Event::where('approved_by', $userModel->id)->get()->toJson()) //TODO: columns definieren
             ->add('webhooks.json', $webhooks)
             ->add(
                 'webhook_creation_requests.json',
-                WebhookCreationRequest::where('user_id', $userModel->id)->get()->toJson()
+                WebhookCreationRequest::where('user_id', $userModel->id)->get()->toJson() //TODO: columns definieren
             )
-            ->add('tokens.json', TokenController::index($userModel)->toJson())
-            ->add('ics_tokens.json', $userModel->icsTokens()->get()->toJson())
+            ->add('tokens.json', TokenController::index($userModel)->toJson()) //TODO: columns definieren
+            ->add('ics_tokens.json', $userModel->icsTokens()->get()->toJson())                                          //TODO: columns definieren
             ->add(
                 'password_resets.json',
-                DB::table('password_resets')->select(['email', 'created_at'])->where('email', $userModel->email)->get()
+                DB::table('password_resets')->select(['email', 'created_at'])->where('email', $userModel->email)->get() //TODO: columns definieren
             )
-            ->add('apps.json', $userModel->oAuthClients()->get()->toJson())
-            ->add('follows.json', DB::table('follows')->where('user_id', $userModel->id)->get())
-            ->add('followings.json', DB::table('follows')->where('follow_id', $userModel->id)->get())
-            ->add('blocks.json', DB::table('user_blocks')->where('user_id', $userModel->id)->get())
-            ->add('mutes.json', DB::table('user_mutes')->where('user_id', $userModel->id)->get())
-            ->add('follow_requests.json', DB::table('follow_requests')->where('user_id', $userModel->id)->get())
-            ->add('follows_requests.json', DB::table('follow_requests')->where('follow_id', $userModel->id)->get())
-            ->add('sessions.json', $userModel->sessions()->get()->toJson())
-            ->add('home.json', $userModel->home()->get()->toJson())
-            ->add('hafas_trips.json', DB::table('hafas_trips')->where('user_id', $userModel->id)->get())
-            ->add('mentions.json', Mention::where('mentioned_id', $userModel->id)->get()->toJson())
-            ->add('roles.json', $userModel->roles()->get()->toJson())
+            ->add('apps.json', $userModel->oAuthClients()->get()->toJson()) //TODO: columns definieren
+            ->add('follows.json', DB::table('follows')->where('user_id', $userModel->id)->get()) //TODO: columns definieren
+            ->add('followings.json', DB::table('follows')->where('follow_id', $userModel->id)->get()) //TODO: columns definieren
+            ->add('blocks.json', DB::table('user_blocks')->where('user_id', $userModel->id)->get()) //TODO: columns definieren
+            ->add('mutes.json', DB::table('user_mutes')->where('user_id', $userModel->id)->get()) //TODO: columns definieren
+            ->add('follow_requests.json', DB::table('follow_requests')->where('user_id', $userModel->id)->get()) //TODO: columns definieren
+            ->add('follows_requests.json', DB::table('follow_requests')->where('follow_id', $userModel->id)->get()) //TODO: columns definieren
+            ->add('sessions.json', $userModel->sessions()->get()->toJson()) //TODO: columns definieren
+            ->add('home.json', $userModel->home()->get()->toJson()) //TODO: columns definieren
+            ->add('hafas_trips.json', DB::table('hafas_trips')->where('user_id', $userModel->id)->get()) //TODO: columns definieren
+            ->add('mentions.json', Mention::where('mentioned_id', $userModel->id)->get()->toJson()) //TODO: columns definieren
+            ->add('roles.json', $userModel->roles()->get()->toJson())                                                             //TODO: columns definieren
             ->add(
                 'activity_log.json',
-                DB::table('activity_log')->where('causer_type', get_class($userModel))->where('causer_id', $userModel->id)->get()
+                DB::table('activity_log')->where('causer_type', get_class($userModel))->where('causer_id', $userModel->id)->get() //TODO: columns definieren
             )
-            ->add('permissions.json', $userModel->permissions()->get()->toJson())
-            ->add('statuses.json', $userModel->statuses()->with('tags')->get())
+            ->add('permissions.json', $userModel->permissions()->get()->toJson()) //TODO: columns definieren
+            ->add('statuses.json', $userModel->statuses()->with('tags')->get()) //TODO: columns definieren
             ->add(
                 'reports.json',
                 DB::table('reports')
                   ->select('subject_type', 'subject_id', 'reason', 'description', 'reporter_id')
                   ->where('reporter_id', $userModel->id)
-                  ->get()
-            )->add('trusted_users.json', DB::table('trusted_users')->where('user_id', $userModel->id)->get());
+                  ->get()                                                       //TODO: columns definieren
+            )
+            ->add('trusted_users.json', DB::table('trusted_users')->where('user_id', $userModel->id)->get()); //TODO: columns definieren
     }
 }
