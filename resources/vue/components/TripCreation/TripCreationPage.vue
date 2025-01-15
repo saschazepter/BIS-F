f8cc403798d3e00c47012c3677543ba4e1cd47fc
Levin <github@17d.me>
2025-01-15 20:07:44 +0100

    metadata layout

---
+++ resources/vue/components/TripCreation/TripCreationPage.vue
@@ -0,0 +1,328 @@
+<script>
+import StationRow from "./StationRow.vue";
+import {DateTime} from "luxon";
+import {trans} from "laravel-vue-i18n";
+
+export default {
+  name: "TripCreationForm",
+  components: {StationRow},
+  mounted() {
+    this.initForm();
+    this.loadOperators();
+  },
+  data() {
+    return {
+      form: {
+        originId: "",
+        originDeparturePlanned: "",
+        destinationId: "",
+        destinationArrivalPlanned: "",
+        lineName: "",
+        journeyNumber: 0,
+        operatorId: "",
+        category: {},
+        stopovers: [],
+      },
+      originTimezone: "Europe/Berlin",
+      destinationTimezone: "Europe/Berlin",
+      stopovers: [],
+      origin: {},
+      destination: {},
+      journeyNumberInput: "",
+      trainTypeInput: "",
+      selectedCategory: {},
+      selectedOperator: null,
+      categories: [
+        {value: "nationalExpress", text: "nationalExpress", emoji: "🚄"},
+        {value: "national", text: "national", emoji: "🚅"},
+        {value: "regionalExp", text: "regionalExpress", emoji: "🚆"},
+        {value: "regional", text: "regional", emoji: "🚞"},
+        {value: "suburban", text: "suburban", emoji: "🚋"},
+        {value: "bus", text: "bus", emoji: "🚌"},
+        {value: "ferry", text: "ferry", emoji: "⛴"},
+        {value: "subway", text: "subway", emoji: "🚇"},
+        {value: "tram", text: "tram", emoji: "🚊"},
+        {value: "taxi", text: "taxi", emoji: "🚖"},
+        {value: "plane", text: "plane", emoji: "✈️"},
+      ],
+      operators: [],
+      disallowed: ["fahrrad", "auto", "fuss", "fuß", "foot", "car", "bike"],
+      showDisallowed: false,
+      validation: {
+        times: null
+      }
+    };
+  },
+  methods: {
+    trans,
+    addStopover() {
+      const dummyStopover = {
+        station: {
+          name: "",
+          id: "",
+        },
+        departurePlanned: "",
+        arrivalPlanned: "",
+      };
+      this.stopovers.push(dummyStopover);
+    },
+    removeStopover(index) {
+      this.stopovers.splice(index, 1);
+      this.validateTimes(); // Optional: Zeiten erneut validieren
+    },
+    setOrigin(item) {
+      this.origin = item;
+      this.form.originId = item.id;
+    },
+    setDeparture(time) {
+      this.form.originDeparturePlanned = DateTime.fromISO(time).setZone(this.originTimezone);
+      this.validateTimes();
+    },
+    setDestination(item) {
+      this.destination = item;
+      this.form.destinationId = item.id;
+    },
+    setArrival(time) {
+      this.form.destinationArrivalPlanned = DateTime.fromISO(time).setZone(this.destinationTimezone);
+      this.validateTimes();
+    },
+    validateTimes() {
+      console.log("Validating times");
+      //iterate over stopovers and destination, check if time is valid
+      let time = DateTime.fromISO(this.form.originDeparturePlanned);
+
+      this.stopovers.forEach((stopover) => {
+        if (time > DateTime.fromISO(stopover.departurePlanned)) {
+          this.validation.times = false;
+          return false;
+        }
+        time = DateTime.fromISO(stopover.arrivalPlanned);
+      });
+
+      if (time > DateTime.fromISO(this.form.destinationArrivalPlanned)) {
+        this.validation.times = false;
+        return false;
+      }
+      this.validation.times = true;
+      return true;
+    },
+    sendForm() {
+      if (!this.validateTimes()) {
+        notyf.error(trans("trip_creation.no-valid-times"));
+        return;
+      }
+
+      this.form.lineName = this.trainTypeInput;
+      this.form.journeyNumber = !isNaN(this.journeyNumberInput) && !isNaN(parseInt(this.journeyNumberInput))
+          ? parseInt(this.journeyNumberInput) : null;
+      this.form.stopovers = this.stopovers.map((stopover) => {
+        return {
+          stationId: stopover.station.id,
+          departure: stopover.departurePlanned,
+          arrival: stopover.arrivalPlanned,
+        };
+      });
+      this.form.category = this.selectedCategory.value;
+
+      fetch("/api/v1/trains/trip", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(this.form),
+      }).then((data) => {
+        if (data.ok) {
+          data.json().then((result) => {
+            result = result.data;
+            let query = {
+              tripId: result.id,
+              lineName: result.lineName,
+              start: result.origin.id,
+              departure: this.form.originDeparturePlanned,
+              idType: 'trwl'
+            };
+
+            window.location.href = `/stationboard?${new URLSearchParams(query).toString()}`;
+          });
+        } else if (data.status === 403 || data.status === 422) {
+          data.json().then((result) => {
+            notyf.error(result.message);
+          });
+        } else {
+          notyf.error(trans("messages.exception.general-values"));
+        }
+      });
+    },
+    setStopoverStation(item, key) {
+      this.stopovers[key].station = item;
+    },
+    setStopoverDeparture(time, key) {
+      this.stopovers[key].departurePlanned = DateTime.fromISO(time).setZone(this.originTimezone);
+      this.validateTimes();
+    },
+    setStopoverArrival(time, key) {
+      this.stopovers[key].arrivalPlanned = DateTime.fromISO(time).setZone(this.destinationTimezone);
+      this.validateTimes();
+    },
+    checkDisallowed() {
+      this.showDisallowed = this.disallowed.some((disallowed) => {
+        return this.trainTypeInput.toLowerCase().includes(disallowed);
+      });
+    },
+    guessModeOfTransport() {
+      // todo: guess mode of transport based on line input
+      // e.g.: if line starts with ICE or TGV, set category to nationalExpress
+    },
+    onLineInput() {
+      this.checkDisallowed()
+      this.guessModeOfTransport();
+    },
+    loadOperators(cursor = null) {
+      fetch("/api/v1/operators?cursor=" + cursor, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+          .then((response) => {
+            if (!response.ok) {
+              throw new Error(response.statusText);
+            }
+            return response.json();
+          })
+          .then((result) => {
+            this.operators.push(...result.data);
+
+            if (result.meta.next_cursor) {
+              this.loadOperators(result.meta.next_cursor);
+            }
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+    },
+    initForm() {
+      this.selectedCategory = this.categories[0];
+    },
+    onChangeCat(event) {
+      console.log(event);
+      console.log(this.selectedCategory)
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="row full-height mt-n4">
+    <div class="col col-md-5 col-lg-4 col-xl-3 p-0 h-100">
+      <div class="accordion accordion-flush border-bottom" id="TripCreationMetaDataAccordion">
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="accordionTripInfo">
+            <button class="accordion-button collapsed" type="button" data-mdb-toggle="collapse"
+                    data-mdb-target="#collapseTripInfo" aria-expanded="false" aria-controls="collapseTripInfo">
+              <div class="d-flex justify-start w-100">
+                <i class="fa-solid fa-list-check"></i>
+                <span class="d-flex justify-content-between w-100 px-2">
+                  <span class="fw-bold" v-if="!trainTypeInput.length">
+                    {{ trans('trip_creation.form.trip_data') }}
+                  </span>
+                  <span class="fw-bold" v-else>
+                    {{ trainTypeInput }}
+                    <span class="fw-lighter fst-italic text-secondary">{{ journeyNumberInput }}</span>
+                  </span>
+                </span>
+              </div>
+            </button>
+          </h2>
+          <div id="collapseTripInfo" class="accordion-collapse collapse" aria-labelledby="accordionTripInfo"
+               data-mdb-parent="#accordionTripInfo">
+            <div class="accordion-body">
+              <input type="text" class="form-control mb-2" :placeholder="trans('trip_creation.form.line')"
+                     :aria-label="trans('trip_creation.form.line')" aria-describedby="basic-addon1"
+                     v-model="trainTypeInput" @focusout="onLineInput"
+              >
+              <input type="text" class="form-control" :placeholder="trans('trip_creation.form.number')"
+                     :aria-label="trans('trip_creation.form.number')" aria-describedby="basic-addon1"
+                     v-model="journeyNumberInput"
+              >
+              <div class="alert alert-danger mt-2" v-show="showDisallowed">
+                <i class="fas fa-triangle-exclamation"></i>
+                {{ trans('trip_creation.limitations.6') }}
+                <a :href="trans('trip_creation.limitations.6.link')" target="_blank">
+                  {{ trans('trip_creation.limitations.6.rules') }}
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="accordionTripCategory">
+            <button class="accordion-button collapsed" type="button" data-mdb-toggle="collapse"
+                    data-mdb-target="#collapseTripCategory" aria-expanded="false" aria-controls="collapseTripCategory">
+              <div class="d-flex justify-start w-100">
+                {{ this.selectedCategory.emoji }}
+                <span class="d-flex justify-content-between w-100 px-2">
+                  <span class="fw-bold">{{ trans('trip_creation.form.travel_type') }}</span>
+                  <span>{{ trans("transport_types." + this.selectedCategory.value) }}</span>
+                </span>
+              </div>
+            </button>
+          </h2>
+          <div id="collapseTripCategory" class="accordion-collapse collapse" aria-labelledby="accordionTripCategory"
+               data-mdb-parent="#accordionTripCategory">
+            <div class="accordion-body">
+              <ul class="list-group">
+                <li v-for="item in categories" class="list-group-item">
+                  <input type="radio" class="form-check-input me-1" name="categoryRadio" :id="item.value" :value="item"
+                         v-model="selectedCategory" @change="onChangeCat">
+                  <label class="form-check-label stretched-link" :for="item.value">
+                    {{ item.emoji }} {{ trans("transport_types." + item.value) }}
+                  </label>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="accordionTripOperator">
+            <button class="accordion-button collapsed" type="button" data-mdb-toggle="collapse"
+                    data-mdb-target="#collapseTripOperator" aria-expanded="false" aria-controls="collapseTripOperator">
+              <div class="d-flex justify-start w-100">
+                <i class="fa-solid fa-building"></i>
+                <span class="d-flex justify-content-between w-100 px-2">
+                  <span class="fw-bold" v-if="selectedOperator == null">
+                    {{ trans('export.title.operator') }}
+                  </span>
+                  <span class="fw-bold" v-else>
+                    {{ selectedOperator.name }}
+                  </span>
+                </span>
+              </div>
+            </button>
+          </h2>
+          <div id="collapseTripOperator" class="accordion-collapse collapse" aria-labelledby="accordionTripOperator"
+               data-mdb-parent="#accordionTripOperator">
+            <div class="accordion-body">
+              <!-- todo: make searchable -->
+              <select class="form-select" v-model="selectedOperator">
+                <option selected>-/-</option>
+                <option v-for="operator in operators" :value="operator">{{ operator.name }}</option>
+              </select>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col d-none d-md-block bg-warning">
+      col 2
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.full-height {
+  min-height: 90vh;
+}
+</style>
