9007f5bdca7c682f817014a93b57a549a69382cc
Levin <github@17d.me>
2025-02-28 15:29:58 +0100

    Prevent identifiers from being created twice

--- app/DataProviders/Repositories/MotisRepository.php
+++ app/DataProviders/Repositories/MotisRepository.php
@@ -6,6 +6,7 @@ use App\Dto\Coordinate;
 use App\Enum\DataProvider;
 use App\Helpers\Formatter;
 use App\Models\Station;
+use App\Models\StationIdentifier;
 use App\Services\GeoService;
 use Illuminate\Support\Collection;
 
@@ -14,22 +15,22 @@ class MotisRepository
     private GeoService $geoService;
     private const string TYPE = 'motis';
 
-    public function __construct(?GeoService $geoService = null) {
+    public function __construct(?GeoService $geoService = null)
+    {
         $this->geoService = $geoService ?? new GeoService();
     }
 
-    public function createStation(mixed $rawStation, DataProvider $source): Station {
+    public function createStation(mixed $rawStation, DataProvider $source): Station
+    {
         $coordinates = new Coordinate($rawStation['lat'], $rawStation['lon']);
-        $bbox        = $this->geoService->getBoundingBox($coordinates, 500);
+        $bbox = $this->geoService->getBoundingBox($coordinates, 500);
 
-        $stations = Station::where('latitude', '>=', $bbox->lowerRight->latitude)
-                           ->where('latitude', '<=', $bbox->upperLeft->latitude)
-                           ->where('longitude', '>=', $bbox->upperLeft->longitude)
-                           ->where('longitude', '<=', $bbox->lowerRight->longitude)
-                           ->get();
+        $stations = Station::whereBetween('latitude', [$bbox->lowerRight->latitude, $bbox->upperLeft->latitude])
+            ->whereBetween('longitude', [$bbox->lowerRight->longitude, $bbox->upperLeft->longitude])
+            ->get();
 
         $simplifiedRawStationName = Formatter::simplifyStationName($rawStation['name']);
-        $stations                 = $stations->map(function($station) use ($simplifiedRawStationName) {
+        $stations = $stations->map(function ($station) use ($simplifiedRawStationName) {
             $stationName = Formatter::simplifyStationName($station->name);
 
             similar_text($stationName, $simplifiedRawStationName, $percent);
@@ -37,40 +38,47 @@ class MotisRepository
             return $station;
         });
 
-        $stations = $stations->filter(function($station) {
+        $stations = $stations->filter(function ($station) {
             return $station->motisRepositoryTempPercent > 90;
         });
+        $stations = $stations->sortBy('ibnr', SORT_ASC);
         $stations = $stations->sortBy('motisRepositoryTempPercent', SORT_ASC);
 
         if ($stations->isEmpty()) {
             $station = new Station([
-                                       'name'      => $rawStation['name'],
-                                       'latitude'  => $rawStation['lat'],
-                                       'longitude' => $rawStation['lon']
-                                   ]);
+                'name' => $rawStation['name'],
+                'latitude' => $rawStation['lat'],
+                'longitude' => $rawStation['lon']
+            ]);
             $station->save();
         } else {
             $station = $stations->first();
         }
 
-        $station->stationIdentifiers()->create([
-                                                   'type'       => self::TYPE,
-                                                   'origin'     => $source->value,
-                                                   'identifier' => $rawStation['stopId'],
-                                                   'name'       => $rawStation['name']
-                                               ]);
+        StationIdentifier::updateOrCreate(
+            [
+                'type' => self::TYPE,
+                'origin' => $source->value,
+                'identifier' => $rawStation['stopId'],
+            ],
+            [
+                'station_id' => $station->id,
+                'name' => $rawStation['name']
+            ]
+        );
         return $station;
     }
 
-    public function getStationsFromDb(string|array $stationIds, DataProvider $source): Collection {
+    public function getStationsByIdentifiers(string|array $stationIds, DataProvider $source): Collection
+    {
         if (is_string($stationIds)) {
             $stationIds = [$stationIds];
         }
 
-        return Station::whereRelation('stationIdentifiers', function($query) use ($stationIds, $source) {
+        return Station::whereRelation('stationIdentifiers', function ($query) use ($stationIds, $source) {
             $query->whereIn('identifier', $stationIds)
-                  ->where('type', static::TYPE)
-                  ->where('origin', $source->value);
+                ->where('type', static::TYPE)
+                ->where('origin', $source->value);
         })->get();
     }
 }
